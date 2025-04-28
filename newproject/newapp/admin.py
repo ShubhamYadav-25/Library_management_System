f17587299aae168c710bb58640c44627e5cdfc24
Shubham Yadav <shub252005@gmail.com>
2025-04-28 14:40:34 +0530

    deleting the unnecessary files

--- newproject/newapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import books, issued_books , users
-
-# Register your models here.
-admin.site.register(books)
-admin.site.register(issued_books)
-admin.site.register(users)
